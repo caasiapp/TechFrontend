30ff69ab731aa122dbdf49d5d5c28ffdb08feefc
Isaac <isaa0396@stud.kea.dk>
2021-12-12 20:04:14 +0100

    .

---
+++ src/main/java/com/example/techfrontend/TechfrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.techfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechfrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechfrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/techfrontend/TechfrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechfrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
